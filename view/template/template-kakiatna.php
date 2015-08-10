48df39c18e3b917cc1f6061da3a739183febe163
rfadli <rianfadli1993@yahoo.com>
2015-08-10 19:06:45 +0700

    ok

---
+++ view/template/template-kakiatna.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Coming Soon</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link href="/public/tools/style.css" rel="stylesheet" type="text/css" />
+<link href="/public/tools/960.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script> 
+<script type="text/javascript" src="/public/js/cufon-yui.js"></script>
+<script type="text/javascript" src="/pubic/js/Clarendon_LT_Std_700.font.js"></script>
+<script type="text/javascript">
+	Cufon.replace('h1,h3', {fontFamily: 'Clarendon LT Std', hover:true})
+</script>
+</head>
+<body>
+<div id="shim"></div>
+
+<?php echo $content; ?>
+
+</body>
+</html>
---
+++ view/welcome/index.php
@@ -0,0 +1,21 @@
+<div id="content">
+	<div class="logo_box"><h1>KAKIATNA<br/>ENERGI</h1></div>          
+	<div class="main_box">
+		<h2>Our website is currently under construction.<br/><span>But you can still reach us from the information below.</span></h2>
+		
+		<ul class="info">
+	        <li>
+				<h3>E</h3>
+				<p>info@kakiatnaenergi.co.id</p>
+			</li>
+            <li>
+				<h3>T</h3>
+				<p>021-2995-1656</p>
+			</li>
+			<li>
+				<h3>A</h3>
+				<p>Gedung The Energy lt. 25, Jl. Jenderal Sudirman Lot 11A, SCBD, Jakarta </p>
+			</li>
+		</ul>
+	</div>
+</div>
