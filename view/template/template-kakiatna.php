e1f99745c00339f99c83dad37e1e4bfa6bec49eb
rfadli <rianfadli1993@yahoo.com>
2015-08-10 19:50:05 +0700

    ok

--- view/template/template-kakiatna.php
+++ view/template/template-kakiatna.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="kakiatna energi, PT.Kakiatna Energi">
 <title>Coming Soon</title>
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 <link href="/public/tools/style.css" rel="stylesheet" type="text/css" />
